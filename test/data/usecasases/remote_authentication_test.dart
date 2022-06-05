06be6464dee79918429bae881821fae783405494
William <178344@upf.br>
2022-06-04 21:09:54 -0300

    test: enusre RemoteAuthentication throws UnexpectedError on 400

--- test/data/usecasases/remote_authentication_test.dart
+++ test/data/usecasases/remote_authentication_test.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:condominioapp/domain/helpers/helpers.dart';
 import 'package:condominioapp/domain/usecases/authentication.dart';
 import 'package:condominioapp/data/http/http.dart';
 import 'package:condominioapp/data/usecases/usecases.dart';
@@ -31,5 +32,21 @@ void main() {
       method: 'post',
       body: {'email': params.email, 'password': params.secret},
     ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(
+            url: url,
+            method: 'post',
+            body: anyNamed("body")))
+        .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      secret: faker.internet.password(),
+    );
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
