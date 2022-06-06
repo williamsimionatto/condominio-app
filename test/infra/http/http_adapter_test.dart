6abfd537f9bc7d5745c1357bcf7f57877d33fd69
williamsimionatto <william.sn0226@gmail.com>
2022-06-06 13:02:45 -0300

    test: ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:condominioapp/data/http/http_error.dart';
+
 import 'package:condominioapp/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -76,6 +78,13 @@ void main() {
 
       final response = await sut.request(url: url, method: 'post');
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
