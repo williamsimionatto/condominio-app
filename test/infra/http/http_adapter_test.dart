c07ebc5c42ba094d9fe25b2f7700e6e5c0e37f59
williamsimionatto <william.sn0226@gmail.com>
2022-06-06 13:06:41 -0300

    test: ensure HttpAdapter throws BadRequestError on 400 with Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -86,5 +86,13 @@ void main() {
       final future = sut.request(url: url, method: 'post');
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('Should return BadRequestError if post returns 400 with body',
+        () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
