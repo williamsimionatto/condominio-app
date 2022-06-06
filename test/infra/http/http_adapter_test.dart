05d17dbde2c958add11e675a8811044db3f4ff2b
williamsimionatto <william.sn0226@gmail.com>
2022-06-06 11:11:34 -0300

    test: ensure HttpAdapter returns null if post returns 200 wihtout data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -23,7 +23,7 @@ class HttpAdapter implements HttpClient {
     final response =
         await client.post(Uri.parse(url), headers: headers, body: jsonBody);
 
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -75,6 +75,15 @@ void main() {
 
       final response = await sut.request(url: url, method: 'post');
       expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if post returns 200 without data', () async {
+      when(client.post(Uri.parse(url),
+              headers: anyNamed('headers'), body: anyNamed('body')))
+          .thenAnswer((_) async => Response('', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+      expect(response, null);
     });
   });
 }
