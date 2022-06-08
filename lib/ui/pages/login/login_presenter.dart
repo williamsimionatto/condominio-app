ee0ff7f8b3f870772664cbfc17e0cd133902a272
williamsimionatto <william.sn0226@gmail.com>
2022-06-08 14:07:17 -0300

    fix: make LoginPresenter auth async

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,12 +1,12 @@
 abstract class LoginPresenter {
   Stream<String> get emailErrorStream;
   Stream<String> get passwordErrorStream;
+  Stream<String> get mainErrorStream;
   Stream<bool> get isFormValidStream;
   Stream<bool> get isLoadingStream;
-  Stream<String> get mainErrorStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
-  void auth();
+  Future<void> auth();
   void dispose();
 }
