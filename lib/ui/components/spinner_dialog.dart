d688ee10684f6e629cd0869771fcb2a665fd2562
williamsimionatto <william.sn0226@gmail.com>
2022-06-08 14:15:10 -0300

    refactor: move SpinnerDialog to its own file

---
+++ lib/ui/components/spinner_dialog.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void showLoading(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return SimpleDialog(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              CircularProgressIndicator(),
+              SizedBox(height: 10),
+              Text('Aguarde ... ', textAlign: TextAlign.center)
+            ],
+          )
+        ],
+      );
+    },
+  );
+}
+
+void hideLoading(BuildContext context) {
+  if (Navigator.canPop(context)) Navigator.pop(context);
+}
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -26,25 +26,9 @@ class _LoginPageState extends State<LoginPage> {
       body: Builder(builder: (context) {
         widget.presenter.isLoadingStream.listen((isLoading) {
           if (isLoading) {
-            showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (BuildContext context) {
-                  return SimpleDialog(
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          CircularProgressIndicator(),
-                          SizedBox(height: 10),
-                          Text('Aguarde ... ', textAlign: TextAlign.center)
-                        ],
-                      )
-                    ],
-                  );
-                });
+            showLoading(context);
           } else {
-            if (Navigator.canPop(context)) Navigator.pop(context);
+            hideLoading(context);
           }
         });
 
