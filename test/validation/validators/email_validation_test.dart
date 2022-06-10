51655d8a45b8278414bbb4e70b0495bb686df781
williamsimionatto <william.sn0226@gmail.com>
2022-06-10 10:44:24 -0300

    test: ensure EmailValidation returns null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,29 @@
+import 'package:condominioapp/validation/protocols/protocols.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  @override
+  final String field;
+
+  EmailValidation(this.field);
+
+  @override
+  String validate(String value) {
+    return null as String;
+  }
+}
+
+void main() {
+  late EmailValidation sut;
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null as String), null);
+  });
+}
