ad7c21ab8e196a4614e75d28f06089571e6ec074
williamsimionatto <william.sn0226@gmail.com>
2022-06-11 16:56:53 -0300

    refactor: move production code to their own files

---
+++ lib/data/usecases/load_current_account/local_load_current_account.dart
@@ -0,0 +1,20 @@
+import 'package:condominioapp/data/cache/cache.dart';
+import 'package:condominioapp/domain/entities/entities.dart';
+import 'package:condominioapp/domain/helpers/domain_error.dart';
+import 'package:condominioapp/domain/usecases/usecases.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({required this.fetchSecureCacheStorage});
+
+  @override
+  Future<AccountEntity> load() async {
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      return AccountEntity(token: token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
+  }
+}
--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -3,28 +3,9 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:condominioapp/domain/entities/entities.dart';
-import 'package:condominioapp/domain/helpers/domain_error.dart';
-import 'package:condominioapp/domain/usecases/usecases.dart';
-
-class LocalLoadCurrentAccount implements LoadCurrentAccount {
-  final FetchSecureCacheStorage fetchSecureCacheStorage;
-
-  LocalLoadCurrentAccount({required this.fetchSecureCacheStorage});
-
-  @override
-  Future<AccountEntity> load() async {
-    try {
-      final token = await fetchSecureCacheStorage.fetchSecure('token');
-      return AccountEntity(token: token);
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class FetchSecureCacheStorage {
-  Future<String> fetchSecure(String key);
-}
+import 'package:condominioapp/domain/helpers/helpers.dart';
+import 'package:condominioapp/data/cache/cache.dart';
+import 'package:condominioapp/data/usecases/usecases.dart';
 
 class FetchSecureStorageSpy extends Mock implements FetchSecureCacheStorage {}
 
