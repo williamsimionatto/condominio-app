469e4ac3efe349dc64ed01a210ebcf93135c7a9d
williamsimionatto <william.sn0226@gmail.com>
2022-06-12 13:04:00 -0300

    test: ensure JWTDecoderAdapter throws if JWTDecoder throws

--- test/infra/jwt/jwt_decoder_adapter_test.dart
+++ test/infra/jwt/jwt_decoder_adapter_test.dart
@@ -1,10 +1,15 @@
+import 'package:condominioapp/domain/helpers/helpers.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 
 class JwtDecoderAdapter {
   bool hasExpired(String token) {
-    return JwtDecoder.isExpired(token);
+    try {
+      return JwtDecoder.isExpired(token);
+    } catch (e) {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -25,5 +30,10 @@ void main() {
 
   test('Should return true when expired token is passed', () {
     expect(sut.hasExpired(mockExpiredToken()), true);
+  });
+
+  test('Should throw exception when invalid token is passed', () {
+    expect(
+        () => sut.hasExpired('invalid_token'), throwsA(DomainError.unexpected));
   });
 }
