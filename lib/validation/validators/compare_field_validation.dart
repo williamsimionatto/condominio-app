2a3365a9f2159847972bab029c51eb7ece83f74f
williamsimionatto <william.sn0226@gmail.com>
2022-06-13 20:16:34 -0300

    test: ensure CompareFieldValidation returns correcty values

---
+++ lib/validation/validators/compare_field_validation.dart
@@ -0,0 +1,19 @@
+import 'package:condominioapp/presentation/protocols/validation.dart';
+
+import '../../validation/protocols/field_validation.dart';
+
+class CompareFieldsValidation implements FieldValidation {
+  @override
+  final String field;
+  final String valueToCompare;
+
+  CompareFieldsValidation({
+    required this.field,
+    required this.valueToCompare,
+  });
+
+  @override
+  ValidationError? validate(String value) {
+    return value == valueToCompare ? null : ValidationError.invalidField;
+  }
+}
---
+++ test/validation/validators/compare_field_validations_test.dart
@@ -0,0 +1,20 @@
+import 'package:condominioapp/presentation/protocols/validation.dart';
+import 'package:condominioapp/validation/validators/validators.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late CompareFieldsValidation sut;
+
+  setUp(() {
+    sut = CompareFieldsValidation(
+        field: 'any_field', valueToCompare: 'any_value');
+  });
+
+  test('Should return error if values are not equal', () {
+    expect(sut.validate('wrong_value'), ValidationError.invalidField);
+  });
+
+  test('Should return null if values are equal', () {
+    expect(sut.validate('any_value'), null);
+  });
+}
