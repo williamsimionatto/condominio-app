40eb1dad921155638d7ef2626e369b9d768feaa4
williamsimionatto <william.sn0226@gmail.com>
2022-06-14 14:46:30 -0300

    refactor: move SUT creation to the setup method

--- test/data/usecases/load_users/remote_load_users_test.dart
+++ test/data/usecases/load_users/remote_load_users_test.dart
@@ -18,11 +18,17 @@ class RemoteLoadUsers {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call HttpClient with correct values', () async {
-    final url = faker.internet.httpUrl();
-    final httpClient = HttpClientSpy();
-    final sut = RemoteLoadUsers(url: url, httpClient: httpClient);
+  late String url;
+  late HttpClient httpClient;
+  late RemoteLoadUsers sut;
 
+  setUp(() {
+    url = faker.internet.httpUrl();
+    httpClient = HttpClientSpy();
+    sut = RemoteLoadUsers(url: url, httpClient: httpClient);
+  });
+
+  test('Should call HttpClient with correct values', () async {
     await sut.load();
     verify(httpClient.request(url: url, method: 'get'));
   });
