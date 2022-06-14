fd220687d11abbd6676468228a65c8b384985674
williamsimionatto <william.sn0226@gmail.com>
2022-06-14 16:09:12 -0300

    test: esnure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -190,5 +190,20 @@ void main() {
       final response = await sut.request(url: url, method: 'get');
       expect(response, null);
     });
+
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if get returns 400 with body',
+        () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
