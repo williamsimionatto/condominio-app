19c43b073662d3a372d31c641782f689f394ba77
williamsimionatto <william.sn0226@gmail.com>
2022-06-14 16:11:28 -0300

    test: esnure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -226,5 +226,12 @@ void main() {
       final future = sut.request(url: url, method: 'get');
       expect(future, throwsA(HttpError.notFound));
     });
+
+    test('Should return ServerError if get returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
