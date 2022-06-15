b5a4e25dc3dda4982328a47fede4149c7be519cf
williamsimionatto <william.sn0226@gmail.com>
2022-06-15 18:18:28 -0300

    refactor: move GetxUsersPresenter to its own file

---
+++ lib/presentation/presenter/getx_users_presenter.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+
+import '../../../ui/pages/pages.dart';
+
+class GetxUsersPresenter {
+  final LoadUsers loadUsers;
+
+  final _isLoading = true.obs;
+  final _users = Rx<List<UserViewModel>>([]);
+
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+  Stream<List<UserViewModel>> get usersStream => _users.stream;
+
+  GetxUsersPresenter({required this.loadUsers});
+
+  Future<void> loadData() async {
+    try {
+      _isLoading.value = true;
+      final users = await loadUsers.load();
+      _users.value = users
+          .map((user) => UserViewModel(
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                active: user.active,
+                cpf: user.cpf,
+              ))
+          .toList();
+    } on DomainError {
+      _users.subject.addError(DomainError.unexpected.description);
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+}
--- test/presentation/presenter/getx_users_presenter_test.dart
+++ test/presentation/presenter/getx_users_presenter_test.dart
@@ -1,43 +1,12 @@
-import 'package:condominioapp/domain/helpers/helpers.dart';
 import 'package:condominioapp/ui/pages/pages.dart';
-import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:condominioapp/domain/helpers/helpers.dart';
 import 'package:condominioapp/domain/entities/entities.dart';
 import 'package:condominioapp/domain/usecases/usecases.dart';
 
-class GetxUsersPresenter {
-  final LoadUsers loadUsers;
-
-  final _isLoading = true.obs;
-  final _users = Rx<List<UserViewModel>>([]);
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<UserViewModel>> get usersStream => _users.stream;
-
-  GetxUsersPresenter({required this.loadUsers});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final users = await loadUsers.load();
-      _users.value = users
-          .map((user) => UserViewModel(
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                active: user.active,
-                cpf: user.cpf,
-              ))
-          .toList();
-    } on DomainError {
-      _users.subject.addError(DomainError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
+import 'package:condominioapp/presentation/presenter/presenter.dart';
 
 class LoadUsersSpy extends Mock implements LoadUsers {}
 
