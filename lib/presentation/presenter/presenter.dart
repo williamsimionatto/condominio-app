21d375b64ace27b476c96c1cfdbc362bcb951783
williamsimionatto <william.sn0226@gmail.com>
2022-06-15 16:02:43 -0300

    refactor: move StreamUsersPresenter to tis own file

--- lib/presentation/presenter/presenter.dart
+++ lib/presentation/presenter/presenter.dart
@@ -1,3 +1,4 @@
 export './stream_login_presenter.dart';
 export './stream_splash_presenter.dart';
 export './stream_signup_presenter.dart';
+export './stream_user_presenter.dart';
--- test/presentation/presenter/stream_users_presenter_test.dart
+++ test/presentation/presenter/stream_users_presenter_test.dart
@@ -1,69 +1,12 @@
-import 'dart:async';
-
-import 'package:condominioapp/domain/helpers/helpers.dart';
 import 'package:condominioapp/ui/pages/pages.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:condominioapp/domain/helpers/helpers.dart';
 import 'package:condominioapp/domain/entities/entities.dart';
 import 'package:condominioapp/domain/usecases/usecases.dart';
 
-class UserState {
-  bool? isLoading = false;
-  late List<UserViewModel>? users;
-  String? mainError;
-}
-
-class StreamUsersPresenter {
-  final LoadUsers loadUsers;
-  final bool? isLoading = true;
-
-  StreamController<UserState>? _controller =
-      StreamController<UserState>.broadcast();
-
-  final _state = UserState();
-
-  Stream<bool?>? get isLoadingStream =>
-      _controller?.stream.map((state) => state.isLoading).distinct();
-
-  Stream<List<UserViewModel>?>? get loadUsersStream =>
-      _controller?.stream.map((state) => state.users).distinct();
-
-  Stream<String?>? get mainErrorStream =>
-      _controller?.stream.map((state) => state.mainError).distinct();
-
-  StreamUsersPresenter({required this.loadUsers});
-
-  Future<void> loadData() async {
-    try {
-      _state.mainError = null;
-      _state.isLoading = true;
-      _update();
-
-      final users = await loadUsers.load();
-      _state.users = users
-          .map(
-            (user) => UserViewModel(
-              id: user.id,
-              name: user.name,
-              email: user.email,
-              active: user.active,
-              cpf: user.cpf,
-            ),
-          )
-          .toList();
-
-      _update();
-    } on DomainError catch (error) {
-      _state.mainError = error.description;
-    } finally {
-      _state.isLoading = false;
-      _update();
-    }
-  }
-
-  void _update() => _controller?.add(_state);
-}
+import 'package:condominioapp/presentation/presenter/presenter.dart';
 
 class LoadUsersSpy extends Mock implements LoadUsers {}
 
