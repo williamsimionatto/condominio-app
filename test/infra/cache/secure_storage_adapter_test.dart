8377c6aa48d14acf93018728acc0ed2170b0d5f3
William Simionatto <william.sn0226@gmail.com>
2022-07-17 14:59:43 -0300

    refactor: extract http client mocks

--- test/infra/cache/secure_storage_adapter_test.dart
+++ test/infra/cache/secure_storage_adapter_test.dart
@@ -1,5 +1,4 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
